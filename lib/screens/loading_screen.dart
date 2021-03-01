eed7df0805eed85630f4d177136d171040fb780a
Ammar <ammarraneez@gmail.com>
2021-03-01 23:33:42 +0530

    navigation to location screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
+import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -10,6 +11,12 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
   double latitude;
   double longitude;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
 
   void getLocationData() async {
     Location location = new Location();
@@ -23,20 +30,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
     double temperature = decodeData['main']['temp'];
     int condition = decodeData['weather'][0]['id'];
     String cityName = decodeData['name'];
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocationData();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
